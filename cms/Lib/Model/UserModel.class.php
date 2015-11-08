55fbf986f326b6e33d32fccec1c0e0f85e9c89c7
a3147972 <2387813033@qq.com>
2015-11-08 22:15:28 +0800

    修改注册推荐

--- cms/Lib/Model/UserModel.class.php
+++ cms/Lib/Model/UserModel.class.php
@@ -144,7 +144,7 @@ class UserModel extends BaseModel
 
         $data_user['add_time'] = $data_user['last_time'] = $_SERVER['REQUEST_TIME'];
         $data_user['add_ip'] = $data_user['last_ip'] = get_client_ip(1);
-        $data_user['recomment'] = $recomment['uid'];
+        $data_user['recomment'] = $check_recomment['uid'];
         $data_user['id_number'] = $id_number;
         $data_user['id_number_img'] = $id_number_img;
         $data_user['with_id_card'] = $with_id_card;
