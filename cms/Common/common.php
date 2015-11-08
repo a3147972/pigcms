8533463791d617170324fd9f609f08d9f30f26ff
a3147972 <2387813033@qq.com>
2015-11-08 21:21:50 +0800

    array_column兼容

--- cms/Common/common.php
+++ cms/Common/common.php
@@ -154,3 +154,19 @@ function create_guid()
     . substr($charid, 20, 12);
     return $uuid;
 }
+
+if (!function_exists('array_column')) {
+    function array_column($array, $column_key, $index_key = null)
+    {
+        return array_reduce($array, function ($result, $item) use ($column_key, $index_key)
+        {
+            if (null === $index_key) {
+                $result[] = $item[$column_key];
+            } else {
+                $result[$item[$index_key]] = $item[$column_key];
+            }
+
+            return $result;
+        }, []);
+    }
+}
